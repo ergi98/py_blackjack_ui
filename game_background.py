30e45f0badc380e813237d1ac130b46067982f8a
ergi98 <edervishaj98@gmail.com>
2025-07-24 17:14:43 +0200

    feat: added animation for game background blur

--- game_background.py
+++ game_background.py
@@ -1,9 +1,9 @@
 from os import path
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt, QPropertyAnimation
-from PyQt6.QtWidgets import QWidget, QLabel, QGraphicsBlurEffect, QGraphicsOpacityEffect
+from PyQt6.QtWidgets import QLabel, QGraphicsBlurEffect, QGraphicsOpacityEffect
 
-class GameBackground(QWidget):
+class GameBackground(QLabel):
   def __init__(self, parent, blur_amount=0):
     super().__init__(parent)
     self.parent = parent
@@ -13,28 +13,22 @@ class GameBackground(QWidget):
     
   def set_background(self):
     self.setGeometry(0, 0, self.parent.width(), self.parent.height())
-    self.background_label = QLabel(self)
-    self.background_label.setGeometry(0, 0, self.parent.width(), self.parent.height())
-    self.background_label.setPixmap(
+    self.setPixmap(
       self.pixmap.scaled(
         self.parent.size(), 
         Qt.AspectRatioMode.KeepAspectRatio, 
         Qt.TransformationMode.SmoothTransformation
       )
     )
-    self.background_label.setScaledContents(True)
+    self.setScaledContents(True)
     # Blur effect
-    blur_effect = QGraphicsBlurEffect(self.parent)
-    blur_effect.setBlurRadius(self.blur_amount)
-    self.background_label.setGraphicsEffect(blur_effect)
-    # Opacity
-    self.opacity_effect = QGraphicsOpacityEffect(self)
-    self.setGraphicsEffect(self.opacity_effect)
+    self.blur_effect = QGraphicsBlurEffect(self.parent)
+    self.blur_effect.setBlurRadius(self.blur_amount)
+    self.setGraphicsEffect(self.blur_effect)
 
-
-  def animate_update_blur(self):
-    self.opacity_animation = QPropertyAnimation(self.opacity_effect, b"opacity")
-    self.opacity_animation.setDuration(1000)
-    self.opacity_animation.setStartValue(1.0)
-    self.opacity_animation.setEndValue(0.0)
-    self.opacity_animation.start()
+  def animate_update_blur(self, updated_value = 0):
+    self.blur_animation = QPropertyAnimation(self.blur_effect, b"blurRadius")
+    self.blur_animation.setDuration(300)
+    self.blur_animation.setStartValue(self.blur_amount)
+    self.blur_animation.setEndValue(updated_value)
+    self.blur_animation.start()
